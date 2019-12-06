5468e63b21830268effd10b36efb7c82b46e240a
Pauline <pauline.windey@gmail.com>
2019-12-06 15:27:21 +0100

    Day2 Part2 successful

--- day2/part2/adventOfCode_day2.2.py
+++ day2/part2/adventOfCode_day2.2.py
@@ -1,29 +1,41 @@
-def intcode(file):
+def findVerbNoun(file):
     listProgram=[]
     fopen = open(file)
     for line in fopen:
         numbers = line.rstrip().split(",")
         for number in numbers:
             listProgram.append(int(number))
-    # restore gravity assist program
-    print(listProgram,"\n")
-    listProgram[1]=12
-    listProgram[2]=2
-    print(listProgram,"\n")
-    l = len(listProgram)-3
-    i=0
-    while i<l:
-        posItem1 = listProgram[i+1]
-        posItem2 = listProgram[i+2]
-        where = listProgram[i+3]
-        if listProgram[i]==1:
-            result = listProgram[posItem1] + listProgram[posItem2]
-            listProgram[where] = result
-        elif listProgram[i]==2:
-            result = listProgram[posItem1] * listProgram[posItem2]
-            listProgram[where] = result
-        i+=4
-    
-    return listProgram
+   
+    for i in range(100):
+        for j in range(100):
+            copyLP = listProgram[:]
+            copyLP[1]=i
+            copyLP[2]=j
+            if intcode(copyLP)==19690720:
+                return 100*i+j
 
-print(intcode("inputDay2.txt")[0])
+   
+def intcode(listProgram):
+    k=0
+    l = len(listProgram)-3
+    while k<l:
+        posItem1 = listProgram[k+1]
+        posItem2 = listProgram[k+2]
+        where = listProgram[k+3]
+        if listProgram[k]==1:
+            result = listProgram[posItem1] + listProgram[posItem2]
+            try:
+                listProgram[where] = result
+            except: 
+                break
+        elif listProgram[k]==2:
+            result = listProgram[posItem1] * listProgram[posItem2]
+            try:
+                listProgram[where] = result
+            except:
+                break
+        k+=4
+    return listProgram[0]
+    
+
+print(findVerbNoun("inputDay2.txt"))
