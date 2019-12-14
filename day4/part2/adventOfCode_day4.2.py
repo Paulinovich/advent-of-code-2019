69124dd96f62e30c80e5db8dbbc60662ca6f3e71
Pauline <pauline.windey@gmail.com>
2019-12-14 20:11:55 +0100

    new try: no errors but not correct

--- day4/part2/adventOfCode_day4.2.py
+++ day4/part2/adventOfCode_day4.2.py
@@ -1,8 +1,6 @@
 def possibilities(x,y):
     pos=0
     for i in range(x,y+1):
-        print(i)
-        print(pos)
         if criteria(i)==True:
             pos+=1
     return pos
@@ -12,35 +10,37 @@ def criteria(number):
     digits=[int(j) for j in str(number)]
     pairs=True
     i=0
-    while i<4:
+    while i<5:
         if digits[i] > digits[i+1]:
-            print("out", number)
             return False
+        # 2 seen
         elif digits[i] == digits[i+1]:
             pairs+=1
-            # look for groups 
             if i<4:
-                print(i)
-                for j in range(2, (6-i)):
-                    #blocks at 222223
-                    print(j)
-                    group=False
-                    if digits[i] < digits[(i+j)]:
-                        i+=j
-                    if digits[i] > digits[(i+j)]:
-                        return False
-                    else: 
-                        group=True
-                # undo added pair from line 17
-                if group==True:
+                if digits[i] > digits[i+2]:
+                    return False
+                elif digits[i] == digits[i+2]:
+                    #3 seen: troup: undo pair
                     pairs-=1
-                    i+=j
-        else:
-            i+=1
-            
+                    if i<3:
+                        if digits[i] > digits[i+3]:
+                            return False
+                        # 4 seen: still chance if i==0
+                        elif digits[i] == digits[i+3]:
+                            if i==0 and digits[i] < digits[i+4]:
+                                i+=5
+                            else:
+                                return False
+                        else:
+                            i+=3
+                    else: 
+                        return False
+                else: 
+                    i+=2
+            else: 
+                return pairs>0
+        else: i+=1
     return pairs>0
-                    
-
-
-# result is too low
+                
 print(possibilities(197487, 673251))
+# returns 1455: not correct
