48b3e66bd6922f1c360444ab01f47060a1dfdf63
Pauline Windey <pauline.windey@gmail.com>
2019-12-16 15:15:31 +0100

    unsuccessful first try

---
+++ day3/part2/adventOfCode_day3.2.py
@@ -0,0 +1,44 @@
+
+
+
+def saveInDict(circuit):
+    # saving positions in dictionary with horizontal positions as key and a list of all vertical positions as value
+    c={}
+    # start position
+    curHor = 0
+    curVert = 0
+    for move in circuit:
+            direction = move[0]
+            steps = int(move[1:])
+            newHor = curHor
+            newVert = curVert
+            if direction == 'R':
+                newHor += steps
+            elif direction == 'L':
+                newHor -= steps
+            elif direction == 'U':
+                newVert += steps
+            elif direction == 'D':
+                newVert -= steps
+            for i in range(min(curHor, newHor), max(curHor, newHor)+1):
+                 for j in range(min(curVert, newVert), max(curVert, newVert)+1):
+                     if i in c:
+                         c[i].append(j)
+                     else: 
+                         c[i]=[j]
+            # update current position
+            curHor = newHor
+            curVert = newVert
+    return c
+
+def getListsCircuits(file):
+    fopen = open(file)
+    lines = []
+    circuits=[]
+    for line in fopen:
+        lines.append(line.rstrip())
+    for line in lines:
+        circuitSolo = []
+        circuitSolo = line.split(",")
+        circuits.append(circuitSolo)
+    return circuits
